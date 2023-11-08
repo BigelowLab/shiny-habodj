92a997c688e2792306d5cd9ab089964be1b0e5d4
jevanilla <jevanilla@bigelow.org>
2023-11-08 14:32:05 -0800

    Initial commit for hab odj shiny dashboard in stand alone repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Harmful algae forecasting through an ocean data justic lense
+
+Nicholas R. Record, Benjamin Tupper, Kenny Douyon, Lauren Drakopulos, Lourdes Vera, Johnathan Evanilla
+
+## Abstract
+
+Forecasting systems for harmful algal blooms (HABs) are becoming more common, as HAB monitoring efforts are increasingly networked and aggregated at national and global levels. Ocean forecasting programs in other fields, however, have been seen to have unintended consequences and out-of-scope uses. The field of Data Justice provides a framework for understanding unintended harms caused by the application of data technologies generally, from predictive policing to technological redlining, and is now starting to be applied to environmental fields. With the proliferation of artificial intelligence algorithms and widespread environmental surveillance, it is timely to turn the data justice lens toward environmental applications, such as the prediction of HABs. This study attempts to orient the HAB forecasting enterprise within the emerging field of Ocean Data Justice. We surveyed three global data repositories underpinning HAB monitoring and prediction efforts: the Ocean Biodiversity Information System, the Harmful Algae Event Database, and AlgaeBase, as well as a literature corpus and the ocean forecasting literature. The patterns we found largely reinforce the existing economic and political relations that underpin global ocean stresses, with monitoring and knowledge generally concentrated in high-GDP, northern North Atlantic nations. 
+
---
+++ app.R
@@ -0,0 +1,170 @@
+## Shiny app designed to explore HAB Ocean Data Justice research results
+
+library(shiny)
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(plotly)
+
+library(markdown)
+
+file <- "odj-obis-haedat_2023-11-8.tsv"
+
+x <- suppressMessages(read_tsv(file))
+
+axis_vars <- c(
+  "Coastline Length (km)" = "coastline (world factbook km)",
+  "EEZ Size km2" = "EEZ km2",
+  "GDP 2020" = "GDP 2020",
+  "HAB Forecast Scholar Hits" = "HAB forecast",
+  "OBIS Records" = "obis_n",
+  "HAEDAT Records" = "haedat_n",
+  "Algaebase Records" = "AlgaeBase hits [marine]"
+)
+
+# lookup table for switching between log10(x) and log10(x+1)
+log_add <- c(
+  "coastline (world factbook km)"=0,
+  "EEZ km2"=0,
+  "GDP 2020"=0,
+  "HAB forecast"=0,
+  "obis_n"=1,
+  "haedat_n"=1,
+  "AlgaeBase hits [marine]" = 1
+)
+
+ui <- navbarPage(
+  title = "HAB ODJ",
+  tabPanel("Home",
+           includeMarkdown("README.md")
+           ),
+  tabPanel("Interactive Plot",
+           sidebarLayout(
+             sidebarPanel("Axis Options",
+               selectInput("x", "X:", axis_vars, selected="GDP 2020"),
+               checkboxInput("x_per_capita", "X per capita", TRUE),
+               checkboxInput("log_x", "Log X", TRUE),
+               selectInput("y", "Y:", axis_vars, selected="obis_n"),
+               checkboxInput("y_per_capita", "Y per capita", FALSE),
+               checkboxInput("log_y", "Log Y", TRUE)
+             ),
+             mainPanel(
+               plotlyOutput("dynamic_plot"),
+               h5(paste("yellow point", "= small island developing state (SIDS)")),
+               h5(paste("blue circle", "= least developed country (LDC)")),
+               h5(paste("Logged database record counts are log10(n+1); all others are log10(n)"))
+             )
+           )),
+  #tabPanel("HAB Record"),
+  tabPanel("Data Table",
+           DT::dataTableOutput("table"))
+)
+
+server <- function(input, output) {
+  
+    output$gdp_obis <- renderPlot({
+      ggplot(data=x, aes(x=log_gdp, y=log_obis)) +
+        geom_point(shape=21, aes(size=1, colour = factor(LDC), fill=factor(`SIDS (1-car, 2-pac, 3-aims)`))) +
+        scale_fill_manual("SID", values = c("gray", "yellow", "yellow", "yellow")) +
+        scale_color_manual(values = c("gray", "blue")) +
+        xlab("GDP per capita (log)") +
+        ylab("OBIS hab records (log)") +
+        stat_cor() +
+        theme_classic() +
+        guides(color=FALSE, size=FALSE, fill=FALSE) 
+    })
+    
+    output$gdp_algaebase <- renderPlot({
+      ggplot(data=x, aes(x=log_gdp, y=log_ab)) +
+        geom_point(shape=21, aes(size=4, colour = factor(LDC), fill=factor(`SIDS (1-car, 2-pac, 3-aims)`))) +
+        scale_fill_manual("SID", values = c("gray", "yellow", "yellow", "yellow")) +
+        scale_color_manual(values = c("gray", "blue")) +
+        xlab("GDP per capita (log)") +
+        ylab("AlgaeBase records (log)") +
+        stat_cor() +
+        theme_classic() +
+        guides(color=FALSE, size=FALSE, fill=FALSE) 
+    })
+    
+    output$gdp_haedat <- renderPlot({
+      ggplot(data=x, aes(x=log_gdp, y=log_hd)) +
+        geom_point(shape=21, aes(size=4, colour = factor(LDC), fill=factor(`SIDS (1-car, 2-pac, 3-aims)`))) +
+        scale_fill_manual("SID", values = c("gray", "yellow", "yellow", "yellow")) +
+        scale_color_manual(values = c("gray", "blue")) +
+        xlab("GDP per capita (log)") +
+        ylab("HAEDAT records (log)") +
+        stat_cor() +
+        theme_classic() +
+        guides(color=FALSE, size=FALSE, fill=FALSE)
+    })
+    
+    ## !!input$x := blah blah blah
+    ## xname = input$
+    ## !!xname := blah
+    ## vignette("programming", package = "dplyr")
+    ## {{ input$x }} := blah
+    
+    plotdata <- reactive({
+      #x variable
+      if (input$log_x & input$x_per_capita) {
+        x <- x |>
+          dplyr::mutate(!!input$x := log10((.data[[input$x]]/x$`Population 2020`)+as.numeric(log_add[[input$x]])))
+      } else if (input$x_per_capita) {
+        x <- x |>
+          dplyr::mutate(!!input$x := .data[[input$x]]/x$`Population 2020`)
+      } else if (input$log_x) {
+        x <- x |>
+          dplyr::mutate(!!input$x := log10(.data[[input$x]]+as.numeric(log_add[[input$x]])))
+      } else {
+        x
+      }
+      
+      #y variable
+      if (input$log_y & input$y_per_capita) {
+        x <- x |>
+          dplyr::mutate(!!input$y := log10((.data[[input$y]]/x$`Population 2020`)+as.numeric(log_add[[input$y]])))
+      } else if (input$y_per_capita) {
+        x <- x |>
+          dplyr::mutate(!!input$y := .data[[input$y]]/x$`Population 2020`)
+      } else if (input$log_y) {
+        x <- x |>
+          dplyr::mutate(!!input$y := log10(.data[[input$y]]+as.numeric(log_add[[input$y]])))
+      } else {
+        x
+      }
+    })
+    
+    
+    output$dynamic_plot <- renderPlotly({
+      
+      xvar_name <- names(axis_vars)[axis_vars == input$x]
+      yvar_name <- names(axis_vars)[axis_vars == input$y]
+      
+      #z <- plotdata()
+      #ct <- cor(x = z[[input$x]], y = z[[input$y]])
+
+      p <- ggplot(data=plotdata(), aes(x = get(input$x), y = get(input$y))) +
+        geom_point(shape=21, aes(size=1, colour = factor(`LDC`), fill=factor(`SIDS (1-car, 2-pac, 3-aims)`), text=paste0("Country:", `Country Name`, sep=" "))) +
+        scale_fill_manual("SID", values = c("gray", "yellow", "yellow", "yellow")) +
+        scale_color_manual(values = c("gray", "blue")) +
+        xlab(xvar_name) +
+        ylab(yvar_name) +
+        stat_cor(na.rm=TRUE, label.x.npc = "center", output.type = "text") +
+        theme_classic() +
+        guides(color=FALSE, size=FALSE, fill=FALSE)
+
+      ggplotly(p, tooltip = "text")
+      
+    })
+    
+    output$table <- DT::renderDataTable(
+      DT::datatable({
+        data <- x
+        data
+      })
+    )
+}
+
+shinyApp(ui = ui, server = server)
